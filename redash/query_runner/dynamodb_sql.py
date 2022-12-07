9d48524994009247cdec7b35c67e393d07cc95f1
Tasneem Rahim <tasneem.rahim@lendable.co.uk>
2022-12-07 17:00:01 +0000

    adding dynamo iam role ability

--- redash/query_runner/dynamodb_sql.py
+++ redash/query_runner/dynamodb_sql.py
@@ -1,15 +1,19 @@
 import logging
 import sys
+import os
 
 from redash.query_runner import *
+from redash.settings import parse_boolean
 from redash.utils import json_dumps
 
 logger = logging.getLogger(__name__)
+ASSUME_ROLE = parse_boolean(os.environ.get("DYNAMO_ASSUME_ROLE", "false"))
 
 try:
     from dql import Engine, FragmentEngine
     from dynamo3 import DynamoDBError
     from pyparsing import ParseException
+    import boto3
 
     enabled = True
 except ImportError as e:
@@ -38,16 +42,33 @@ class DynamoDBSQL(BaseSQLQueryRunner):
 
     @classmethod
     def configuration_schema(cls):
-        return {
+        schema = {
             "type": "object",
             "properties": {
                 "region": {"type": "string", "default": "us-east-1"},
                 "access_key": {"type": "string"},
                 "secret_key": {"type": "string"},
             },
-            "required": ["access_key", "secret_key"],
+            "required": [],
             "secret": ["secret_key"],
         }
+        if ASSUME_ROLE:
+            del schema["properties"]["aws_access_key"]
+            del schema["properties"]["aws_secret_key"]
+            schema["secret"] = []
+            schema["properties"].update(
+                {
+                    "iam_role": {"type": "string", "title": "IAM role to assume"},
+                    "external_id": {
+                        "type": "string",
+                        "title": "External ID to be used while STS assume role",
+                    },
+                }
+            )
+        else:
+            schema["required"] = ["aws_access_key", "aws_secret_key"]
+        
+        return schema
 
     def test_connection(self):
         engine = self._connect()
@@ -60,6 +81,28 @@ class DynamoDBSQL(BaseSQLQueryRunner):
     @classmethod
     def name(cls):
         return "DynamoDB (with DQL)"
+
+    def _get_iam_credentials(self, user=None):
+        if ASSUME_ROLE:
+            role_session_name = "redash" if user is None else user.email
+            sts = boto3.client("sts")
+            creds = sts.assume_role(
+                RoleArn=self.configuration.get("iam_role"),
+                RoleSessionName=role_session_name,
+                ExternalId=self.configuration.get("external_id"),
+            )
+            return {
+                "aws_access_key_id": creds["Credentials"]["AccessKeyId"],
+                "aws_secret_access_key": creds["Credentials"]["SecretAccessKey"],
+                "aws_session_token": creds["Credentials"]["SessionToken"],
+                "region_name": self.configuration["region"],
+            }
+        else:
+            return {
+                "aws_access_key_id": self.configuration.get("aws_access_key", None),
+                "aws_secret_access_key": self.configuration.get("aws_secret_key", None),
+                "region_name": self.configuration["region"],
+            }
 
     def _connect(self):
         engine = FragmentEngine()
